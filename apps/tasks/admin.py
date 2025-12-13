2e64eb43cb856ada9ea3b160738a9ebd6cb68fa1
Said <pm5581287@gmail.com>
2025-12-13 20:48:16 +0300

    add TaskAdmin and ProjectAdmin

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+  list_display = ['title', 'description', 'project',
+                  'assignee', 'status', 'created_by']
+  readonly_fields = ['created_at', 'updated_at']

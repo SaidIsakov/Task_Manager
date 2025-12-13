e5c198add3042071b79c2d087b7e15e8b564a7dc
Said <pm5581287@gmail.com>
2025-12-13 19:25:34 +0300

    create ProjectSerializer/ProjectViewSet

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProjectSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Project
 
-# Create your views here.
+class ProjectViewSet(ModelViewSet):
+  queryset = Project.objects.all()
+  serializer_class = ProjectSerializer
+  permission_classes = [IsAuthenticated]
+
+  def perform_create(self, serializer):
+    """
+    Автоматически связывает нашего пользователя с новым проектом
+    """
+    serializer.save(owner=self.request.user)
+

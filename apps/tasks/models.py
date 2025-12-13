a78325b7cf75a733aa32dfe6fc5849beca0260ab
Said <pm5581287@gmail.com>
2025-12-13 19:03:38 +0300

    create new app(tasks) and create model(Task)

---
+++ apps/tasks/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TaskStatus(models.TextChoices):
+  NEW = 'new', 'New',
+  IN_PROGRESS = 'in_progress', 'In_progress',
+  DONE = 'done', 'Done'
+
+
+class Task(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.TextField(blank=True)
+  assignee = models.ForeignKey(
+    User,
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    related_name='tasks'
+  )
+  status = models.CharField(
+    max_length=20,
+    choices=TaskStatus.choices,
+    default = TaskStatus.NEW
+  )
+
+  created_by = models.ForeignKey(
+    User,
+    on_delete=models.CASCADE,
+    related_name='created_tasks'
+  )
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return self.title
+  
--- conf/settings.py
+++ conf/settings.py
@@ -24,7 +24,8 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
-  'apps.projects'
+  'apps.projects',
+  'apps.tasks',
 ]
 
 MIDDLEWARE = [

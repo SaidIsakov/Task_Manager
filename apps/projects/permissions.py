b3121c3aaf7d989ed8049c36cec43380b924bad6
Said <pm5581287@gmail.com>
2025-12-16 12:35:30 +0300

    create new method for ProjectViewSet(get_permissions)

--- apps/projects/permissions.py
+++ apps/projects/permissions.py
@@ -1,11 +1,50 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.permissions import SAFE_METHODS
+from .models import ProjectMember, ProjectRole
+
+
+def get_project_membership(user, project):
+    """
+    Возвращает ProjectMember или None
+    """
+    return ProjectMember.objects.filter(
+        user=user,
+        project=project,
+        is_active=True
+    ).first()
+
+
+class IsProjectOwnerOrAdmin(BasePermission):
+  def has_object_permission(self, request, view, obj):
+    membership = get_project_membership(request.user, obj)
+    if not membership:
+      return False
+
+    return membership.role in [
+            ProjectRole.OWNER,
+            ProjectRole.ADMIN
+        ]
+
+
+class IsMember(BasePermission):
+  """ Участник """
+  def has_object_permission(self, request, view, obj):
+     return ProjectMember.objects.filter(
+            user=request.user,
+            project=obj,
+            is_active=True
+        ).exists()
+
 
 class IsProjectOwner(BasePermission):
-  pass
-  # """
-  #   Разрешает доступ только владельцу проекта.
-  #   SAFE_METHODS (GET, HEAD, OPTIONS) тоже проверяются, но можно разрешить только владельцу.
-  #   """
-  # def get_object_permission(self, request, view, obj):
-  #   return obj.owner == request.user
+  """
+  Только OWNER
+  """
+
+  def has_object_permission(self, request, view, obj):
+    membership = get_project_membership(request.user, obj)
+
+    if not membership:
+        return False
+
+    return membership.role == ProjectRole.OWNER
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -2,19 +2,25 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import ProjectSerializer
 from rest_framework.permissions import IsAuthenticated
 from .models import Project
-from .permissions import IsProjectOwner
+from .permissions import IsProjectOwnerOrAdmin, IsMember, IsProjectOwner
 from django.db.models import Q
 
 
 class ProjectViewSet(ModelViewSet):
   serializer_class = ProjectSerializer
-  permission_classes = [IsAuthenticated, IsProjectOwner]
 
   def perform_create(self, serializer):
     """
     Автоматически связывает нашего пользователя с новым проектом
     """
     serializer.save(owner=self.request.user)
+
+  def get_permissions(self):
+    if self.action in ['update', 'partial_update']:
+      return [IsProjectOwnerOrAdmin()]
+    if self.action == 'destroy':
+      return [IsProjectOwner()]
+    return [IsMember()]
 
   def get_queryset(self):
     """ Проеты видит толко участник и владелец """
